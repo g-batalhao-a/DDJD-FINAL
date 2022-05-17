bdbace285103a421d789ca4aeee49342fbf0d564
DanielGSilva <dg.sarmento2000@gmail.com>
2022-05-17 11:02:49 +0100

    Restrict player row to 1s

--- Assets/Scripts/2v2Rowing/Boat1Movement.cs
+++ Assets/Scripts/2v2Rowing/Boat1Movement.cs
@@ -7,44 +7,142 @@ public class Boat1Movement : MonoBehaviour
 {
     public Vector3 leftCenter, rightCenter;
     
-    public float speed = 100f;
+    public float leftSpeed = 0f, rightSpeed = 0f, leftAcceleration = 0f, rightAcceleration = 0f;
     
-    private Bounds bounds;
+    public float leftPressed = 0f, rightPressed = 0f;
+    
     private GameControlls gameControlls;
+    
+    private const float ACCELERATION_STEP = 5f;
+    private const float MAX_ACCELERATION = 90f;
+    private const float MIN_ACCELERATION = -240f;
+    private const float MAX_SPEED = 40f;
     
     // Start is called before the first frame update
     void Start()
     {
-        gameControlls = new GameControlls();
-        
-        gameControlls.Player1.Enable();
-        gameControlls.Player2.Enable();
-        
-        getLeftCenter();
-        getRightCenter();
+        setupControlls();
+        updateAnchorPoints();
     }
 
     // Update is called once per frame
     void Update()
     {
+        updateAnchorPoints();
+        updateAccelerations(); 
+        updateSpeeds(Time.deltaTime);
+        updatePositions(Time.deltaTime);
+    }
+    
+    private void setupControlls() {
+        gameControlls = new GameControlls();
+        
+        gameControlls.Player1.Enable();
+        gameControlls.Player2.Enable();
+    }
+    
+    private void updateAnchorPoints() {
         getLeftCenter();
         getRightCenter();
-        
-        
-        if (gameControlls.Player1.Action.IsPressed()) {
-            transform.RotateAround(rightCenter, transform.up, speed * Time.deltaTime);
-        }
-        
-        if (gameControlls.Player2.Action.IsPressed()) {
-            transform.RotateAround(leftCenter, -transform.up, speed * Time.deltaTime);
-        }
     }
     
     private void getLeftCenter() {
-        leftCenter = transform.GetChild(0).position;
+        leftCenter = transform.GetChild(0).position; // 1st child of Boat GameObject should always be leftAnchor
     }
     
     private void getRightCenter() {
-        rightCenter = transform.GetChild(1).position;
+        rightCenter = transform.GetChild(1).position; // 2nd child of Boat GameObject should always be rightAnchor
+    }
+    
+    private void updateAccelerations() {
+        if (gameControlls.Player1.Action.IsPressed()) {
+            leftPressed += Time.deltaTime;
+            
+            // only accelerates for 1s, then player must re press the button to accelerate again
+            if (leftPressed >= 1f) {
+                leftDeaccelerate();
+            } else {
+                leftAccelerate();
+            }
+        } else {
+            leftPressed = 0f;
+            leftDeaccelerate();
+        }
+        
+        if (gameControlls.Player2.Action.IsPressed()) {
+            rightPressed += Time.deltaTime;
+            
+            // only accelerates for 1s, then player must re press the button to accelerate again
+            if (rightPressed >= 1f) {
+                rightDeaccelerate();
+            } else {
+                rightAccelerate();
+            }
+        } else {
+            rightPressed = 0f;
+            rightDeaccelerate();
+        }
+    }
+    
+    private void leftAccelerate() {
+        leftAcceleration += ACCELERATION_STEP;
+        if (leftAcceleration > MAX_ACCELERATION) {
+            leftAcceleration = MAX_ACCELERATION;
+        }
+    }
+    
+    private void leftDeaccelerate() {
+        leftAcceleration -= ACCELERATION_STEP;
+        if (leftAcceleration < MIN_ACCELERATION) {
+            leftAcceleration = MIN_ACCELERATION;
+        }
+    }
+    
+    private void rightAccelerate() {
+        rightAcceleration += ACCELERATION_STEP;
+        if (rightAcceleration > MAX_ACCELERATION) {
+            rightAcceleration = MAX_ACCELERATION;
+        }
+    }
+    
+    private void rightDeaccelerate() {
+        rightAcceleration -= ACCELERATION_STEP;
+        if (rightAcceleration < MIN_ACCELERATION) {
+            rightAcceleration = MIN_ACCELERATION;
+        }
+    }
+    
+    private void updateSpeeds(float deltaTime) {
+        updateLeftSpeed(deltaTime);
+        updateRightSpeed(deltaTime);
+    }
+    
+    private void updateLeftSpeed(float deltaTime) {
+        leftSpeed += leftAcceleration * deltaTime;
+        if (leftSpeed > MAX_SPEED) {
+            leftSpeed = MAX_SPEED;
+            return;
+        }
+        if (leftSpeed < 0f) {
+            leftSpeed = 0f;
+            leftAcceleration = 0f;
+        }
+    }
+    
+    private void updateRightSpeed(float deltaTime) {
+        rightSpeed += rightAcceleration * deltaTime;
+        if (rightSpeed > MAX_SPEED) {
+            rightSpeed = MAX_SPEED;
+            return;
+        }
+        if (rightSpeed < 0f) {
+            rightSpeed = 0f;
+            rightAcceleration = 0f;
+        }
+    }
+    
+    private void updatePositions(float deltaTime) {
+        transform.RotateAround(rightCenter, transform.up, leftSpeed * deltaTime);
+        transform.RotateAround(leftCenter, -transform.up, rightSpeed * deltaTime);
     }
 }
